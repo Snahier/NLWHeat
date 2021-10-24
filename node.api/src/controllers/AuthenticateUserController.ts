ca3725d44398158d176a8f8c1ae03d1cd44b4905
Snahier <hansalmeida@hotmail.com>
2021-10-24 07:51:03 -0300

    Add User Controller exception handle

--- node.api/src/controllers/AuthenticateUserController.ts
+++ node.api/src/controllers/AuthenticateUserController.ts
@@ -6,8 +6,13 @@ export class AuthenticateUserController {
     const { code } = request.body
 
     const service = new AuthenticateUserService()
-    const result = await service.execute(code)
 
-    return response.json(result)
+    try {
+      const result = await service.execute(code)
+
+      return response.json(result)
+    } catch (error) {
+      return response.json({ error: error.message })
+    }
   }
 }
