313069341c63f7111d2db58a90b1654769428439
Snahier <hansalmeida@hotmail.com>
2021-10-24 08:43:59 -0300

    Add message service socket.io integration

--- node.api/src/services/CreateMessageService.ts
+++ node.api/src/services/CreateMessageService.ts
@@ -1,3 +1,4 @@
+import { io } from "../app"
 import { prismaClient } from "../prisma"
 
 export class CreateMessageService {
@@ -8,6 +9,18 @@ export class CreateMessageService {
         user: true,
       },
     })
+
+    const webSocketResponseData = {
+      text: message.text,
+      user_id: message.user_id,
+      created_at: message.created_at,
+      user: {
+        name: message.user.name,
+        avatar_url: message.user.avatar_url,
+      },
+    }
+
+    io.emit("new_message", webSocketResponseData)
 
     return message
   }
