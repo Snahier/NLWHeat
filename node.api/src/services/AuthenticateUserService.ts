1e7628ace29559be3f48c941283b552810d5e2b5
Snahier <hansalmeida@hotmail.com>
2021-10-24 07:41:21 -0300

    Add find or create user logic

--- node.api/src/services/AuthenticateUserService.ts
+++ node.api/src/services/AuthenticateUserService.ts
@@ -32,6 +32,23 @@ export class AuthenticateUserService {
       },
     })
 
-    return response.data
+    const { login, id, avatar_url, name } = response.data
+
+    let user = await prismaClient.user.findFirst({
+      where: {
+        github_id: id,
+      },
+    })
+
+    if (!user) {
+      user = await prismaClient.user.create({
+        data: {
+          github_id: id,
+          login,
+          avatar_url,
+          name,
+        },
+      })
+    }
   }
 }
