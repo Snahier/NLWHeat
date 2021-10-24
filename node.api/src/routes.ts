add499aa9cd88d19498286f2bf24d4ead1329ade
Snahier <hansalmeida@hotmail.com>
2021-10-24 08:31:15 -0300

    Crete route: send message

--- node.api/src/routes.ts
+++ node.api/src/routes.ts
@@ -1,6 +1,10 @@
 import { Router } from "express"
+import { ensureAuthenticated } from "../prisma/middleware/ensureAuthenticated"
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController"
+import { CreateMessageController } from "./controllers/CreateMessageController"
 
 export const router = Router()
 
 router.post("/authenticate", new AuthenticateUserController().handle)
+
+router.post("/messages", ensureAuthenticated, new CreateMessageController().handle)
