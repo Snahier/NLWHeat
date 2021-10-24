00c54eba99962854b6237680f96692e625d7e2ae
Snahier <hansalmeida@hotmail.com>
2021-10-24 06:41:55 -0300

    Create github oauth routes

--- node.api/src/main.ts
+++ node.api/src/main.ts
@@ -3,5 +3,15 @@ import express from "express"
 
 const app = express()
 
+app.get("/github", (request, response) => {
+  response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
+})
+
+app.get("/signin/callback", (request, response) => {
+  const { code } = request.query
+
+  return response.json(code)
+})
+
 const port = 4000
 app.listen(port, () => console.log(`Runing at: http://localhost:${port}`))
