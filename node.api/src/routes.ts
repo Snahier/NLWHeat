9e36bef291dfc15045b67482a6bf749087a78706
Snahier <hansalmeida@hotmail.com>
2021-10-24 08:58:53 -0300

    Create route: User profile

--- node.api/src/routes.ts
+++ node.api/src/routes.ts
@@ -3,6 +3,7 @@ import { ensureAuthenticated } from "../prisma/middleware/ensureAuthenticated"
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController"
 import { CreateMessageController } from "./controllers/CreateMessageController"
 import { GetLast3MessagesController } from "./controllers/GetLast3MessagesController"
+import { UserProfileController } from "./controllers/UserProfileController"
 
 export const router = Router()
 
@@ -10,3 +11,5 @@ router.post("/authenticate", new AuthenticateUserController().handle)
 
 router.post("/messages", ensureAuthenticated, new CreateMessageController().handle)
 router.get("/messages/last3", new GetLast3MessagesController().handle)
+
+router.get("/profile", ensureAuthenticated, new UserProfileController().handle)
