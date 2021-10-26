40cd3045f13afcc53c7e6b33982e5cd4832ae410
Snahier <hansalmeida@hotmail.com>
2021-10-25 21:19:56 -0300

    Add theme colors and fonts

--- mobile/src/styles/theme.ts
+++ mobile/src/styles/theme.ts
@@ -1,5 +1,22 @@
 // prettier-ignore
 export const defaultTheme = {
-  background: "#000000",
-  text:       "#ffffff",
+  white:          "#FFFFFF",
+  green:          "#1B873F",
+  pink:           "#FF008E",
+  orange:         "#FF7A29",
+  yellow:         "#FFCD1E",
+
+  blackPrimary:   "#09090A",
+  blackSecondary: "#121214",
+  blackTertiary:  "#202024",
+
+  grayPrimary:    "#8D8D99",
+  graySecondary:  "#C4C4CC",
+  grayTertiary:   "#E1E1E6",
+  grayQuaternary: "#29292E",
+
+  fonts: {
+    regular: 'Roboto_400Regular',
+    bold: 'Roboto_700Bold',
+  }
 }
